17ea88cf446c5c45572aa5ba661bb07f8344db11
NoAngel <155915830+NopAngel@users.noreply.github.com>
2024-06-27 16:02:11 -0400

    navbar > new configs..

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,10 +4,10 @@ import React from "react";
 function Sidebar() {
   return (
     <nav
-      className="sticky top-0 z-20 bg-[rgba(26,25,27,0.44)] backdrop-blur-xl w-[200px] border-neutral-800 mx-auto rounded-xl mt-[10px] mb-[10px]"
+      className="sticky top-[20px] z-20 bg-[rgba(26,25,27,0.44)] backdrop-blur-xl w-[200px] border-neutral-800 mx-auto rounded-xl mt-[10px] mb-[10px]"
       id="anim-dv"
     >
-      <ul className="flex justify-center *:p-[6px] *:text-2xl">
+      <ul className="flex justify-around *:p-[6px] *:text-2xl">
         <li>
           <a href="/">
             <FiHome />
