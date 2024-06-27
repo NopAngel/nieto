d912023853299fa82c6418592999647148e52c39
NoAngel <155915830+NopAngel@users.noreply.github.com>
2024-06-27 15:55:56 -0400

    navbar.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Sidebar() {
   return (
     <nav
-      className="sticky top-6 z-20 bg-[rgba(26,25,27,0.44)] backdrop-blur-sm w-[200px] border-neutral-800 mx-auto rounded-xl"
+      className="sticky top-0 z-20 bg-[rgba(26,25,27,0.44)] backdrop-blur-xl w-[200px] border-neutral-800 mx-auto rounded-xl mt-[10px] mb-[10px]"
       id="anim-dv"
     >
       <ul className="flex justify-center *:p-[6px] *:text-2xl">
