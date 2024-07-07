c1b19e83bad73ca3da688d2b34953e31308fc3de
NoAngel <155915830+NopAngel@users.noreply.github.com>
2024-07-07 14:44:11 -0400

    Update card_projects.tsx

--- components/projects/card_projects.tsx
+++ components/projects/card_projects.tsx
@@ -35,7 +35,7 @@ const Card = ({
         <article className="flex gap-2">
           <LinkPreview url={web}>
             <section className="flex">
-              {tags.map((i, index): TagElement => (
+              {tags.map((i, index) => (
                 <div
                   key={index}
                   className="bg-zinc-950/55 backdrop-blur-sm rounded-xl border-2 border-zinc-900 p-1 font-mono"
