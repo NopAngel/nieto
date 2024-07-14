4136a8e0d53f760b087ad2a73d57d4f503b1a6cd
NoAngel <155915830+NopAngel@users.noreply.github.com>
2024-07-14 14:36:18 -0400

    work-section :: edit

--- app/components/profile-two.tsx
+++ app/components/profile-two.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function ProfileTwo() {
   return (
-    <section className="flex gap-2 items-center justify-center mt-6">
+    <section className="flex xl:flex-row flex-col gap-2 items-center justify-center mt-6">
       <article className="text-left">
         <h1 className="text-3xl font-bold">Angel Nieto</h1>
         <h2 className="p-2 opacity-80">Developer Front-End (Designer Dev.).</h2>
--- app/components/section/work-section.tsx
+++ app/components/section/work-section.tsx
@@ -5,22 +5,24 @@ export default function Work() {
     <section className="flex justify-center mt-6 mx-auto text-left">
       <article>
         <h1 className="font-semibold text-xl">Work</h1>
-        <p className="max-w-[500px] p-2">
+        <div className="w-[600px] mx-auto">
+        <p className=" p-2">
           Hi, there!, my name is Angel Nieto, Developer Front-End and Developer
           Golang.{" "}
         </p>
-        <p className="max-w-[500px] p-2">
+        <p className="p-2">
           I started in 2022 with my first HTML learning, then I learned other
           languages, entering my passion into development a little more, until
           now, in 2024, I have created many important projects for my
           development and growth.
         </p>
-        <p className="p-2 max-w-[500px]">
+        <p className="p-2">
           Learning Development in Go (Golang), creating several projects,
           although right now I am learning this language (very open), I learned
           a Framework called Svelte, developing several open projects with
           Svelte to continue advancing much further.
         </p>
+        </div>
         <div className="w-[200px] mx-auto mt-2">
           <Button link="/work">My Portfolio</Button>
         </div>
