e4dabf7142b3605ea0ca58d821814c7a3e99a38c
NoAngel <155915830+NopAngel@users.noreply.github.com>
2024-07-14 14:31:40 -0400

    app :: portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Angel Nieto | Developer Front-End",
   description: "Developer Front-End and Golang Developer - Angel Nieto",
-  icons: {
-    icon: "@/favicon.ico"
-  }
 };
 
 export default function RootLayout({
