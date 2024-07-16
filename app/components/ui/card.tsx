79516b3d77e0beb1dc9bbd4223ea87c17e30bd5e
NoAngel <155915830+NopAngel@users.noreply.github.com>
2024-07-15 22:41:05 -0400

    Card UI || Card Setting Complete.

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Card({ Title, Img, Description, Href }: any) {
   return (
-    <a href={Href}>
+    <a href={Href} target="_blank">
       <article className="bg-transparent/55 h-[250px] shadow-sm rounded-sm p-2">
         <img src={Img} className="w-[400px]" alt={Title} />
         <section>
